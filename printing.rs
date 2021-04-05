2aa9354a8fb8bee34459aee45ff44a7f99791617
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-05 20:35:42 +0300

    more formatting

--- printing.rs
+++ printing.rs
@@ -30,4 +30,15 @@ fn main() {
     println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
     println!("{0} in octa: {0:o}", 12); // Octal
     println!("{0} in hexa: {0:x}", 12); // Hexadecimal
+
+    // Controlling the width of the output
+    println!("{n:w$}|", n=3, w=7); // right-aligned (6 spaces and a `3`)
+    println!("{n:>7}|", n=3); // right-aligned (6 spaces and a `3`)
+    println!("{n:<7}|", n=3); // left-aligned (6 spaces and a `3`)
+    println!("{n:<07}|", n=3); // left-aligned (6 zeros and a `3`)
+
+    // Debugging
+    println!("This format is used for display: {}", "What's changed?");
+    println!("This format is used for debugging: {:?}", "What's changed?");
+    println!("This format is used for debugging: {:?}", 3.14);
 }
