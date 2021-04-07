136d80e8a45f05361ff122d24fd24e48dca45cde
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-07 23:39:13 +0300

    guessing game: read input

--- project/guessing_game/src/main.rs
+++ project/guessing_game/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
+fn get_guess() -> String {
+    println!("Enter your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    return guess;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+   let guess = get_guess();
+   println!("Your guess: {}", guess);
 }
