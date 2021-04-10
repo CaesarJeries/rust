b0698c63a9d22c6ad8659af2cff17b954a2c0c07
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-04-10 04:36:45 +0300

    arrays: added case of uninitialized array

--- arrays.rs
+++ arrays.rs
@@ -11,4 +11,8 @@ fn main() {
 
     // initialize an array of size 5 filled with 3's
     let _a3 = [3; 5];
+
+    let a4: [i8; 13]; // creates an uninitialized array. This actually compiles
+    //println!("a4[0]: {}", a4[0]); // but if you try to access an element, you'll
+                                    // get an error[E0381]: use of possibly-uninitialized variable
 }
